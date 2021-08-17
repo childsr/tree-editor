076fb1506cb17e6ba6e44a9d7745314acde288c9
Ryan Childs <rchilds24@gmail.com>
2021-08-17 09:58:30 -0600

    Initial commit

---
+++ scripts/prod.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const { join } = require("path")
+const { readFileSync, writeFileSync, copyFileSync } = require("fs")
+const { createInterface } = require("readline")
+const { spawn } = require("child_process")
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+
+const bumpPatchVersion = version => {
+  const [maj, min, pat] = version.split(".")
+  return [maj, min, (parseInt(pat) + 1).toString()].join(".")
+}
+const bumpMinorVersion = version => {
+  const [maj, min] = version.split(".")
+  return [maj, (parseInt(min) + 1).toString(), "0"].join(".")
+}
+const bumpMajorVersion = version => {
+  const [maj] = version.split(".")
+  return [(parseInt(maj) + 1).toString(), "0", "0"].join(".")
+}
+
+const getPath = relativePath => join(__dirname, "..", relativePath)
+
+let packageJson = JSON.parse(readFileSync(getPath("package.json"), "utf8"))
+const { name, version } = packageJson
+
+rl.question(`Current version of this package is ${version}.\nIs this a major(u) update, minor(m) update, or a patch(p)? [u/m/p] (default: "p") `, ans => {
+  let bump
+  if (ans.toLowerCase() === "u") bump = bumpMajorVersion
+  else if (ans.toLowerCase() === "m") bump = bumpMinorVersion
+  else bump = bumpPatchVersion
+
+  const nextVersion = bump(version)
+  rl.question(`Bump version to ${nextVersion} and publish "${name}"?[Y/n] `, ans => {
+    if (ans.toLowerCase() === "n") process.exit()
+    else publish(nextVersion)
+  })
+})
+
+
+function publish(version) {
+  console.log("publishing package")
+
+  Object.assign(packageJson, { version })
+  writeFileSync(getPath("dist/package.json"), JSON.stringify(packageJson, null, 2))
+  writeFileSync(getPath("package.json"), JSON.stringify(packageJson, null, 2))
+  copyFileSync(getPath("LICENSE"), getPath("./dist/LICENSE"))
+
+  spawn("npm", ["publish", getPath("dist"), "--access", "public"/* , "--dry-run" */], { stdio: ["inherit", "inherit", "inherit"] })
+    .on("close", process.exit)
+}
---
+++ src/index.ts
@@ -0,0 +1,178 @@
+export interface Tree {
+  mount(parent: Element): void
+  unmount(): void
+  root: TreeNode
+}
+export interface TreeNode {
+  content: string
+  collapsed: boolean
+  readonly children: TreeNode[]
+  add(childHtml: string, index?: number): void
+  remove(index: number): void
+  _el: HTMLDivElement
+}
+export type TreeParams = {
+  indent: string
+  fontFamily: string
+  fontWeight: string
+  fontSize: string
+  baseFontSize: string
+  id: string
+  collapsedBullet: string
+  expandedBullet: string
+}
+export type TreeData = string | [content: string, children: TreeData[]]
+ 
+const defaults: Partial<TreeParams> = {
+  indent: "10px",
+  fontFamily: "monospace",
+  fontWeight: "normal",
+  fontSize: "12pt",
+  expandedBullet: "- ",
+  collapsedBullet: "+ "
+}
+
+const compile = (html: string): HTMLElement => {
+  const div = document.createElement("div")
+  div.innerHTML = html
+  return div.children[0] as HTMLElement
+}
+const treeNode = (el: HTMLDivElement): TreeNode => {
+  const childrenElement = el.querySelector(".children") as HTMLDivElement
+  const contentEl = el.querySelector(".content") as HTMLSpanElement
+  let content = contentEl.innerText
+  let children: TreeNode[]
+  const setChildren = () => {
+    children = [...childrenElement.children].map(treeNode as any) as TreeNode[]
+  }
+  setChildren()
+  return {
+    get content() { return content },
+    set content(c) {
+      content = c
+      contentEl.innerText = content
+    },
+    get collapsed() { return childrenElement.classList.contains("collapsed") },
+    set collapsed(c) {
+      if (c) {
+        childrenElement.classList.add("collapsed")
+        el.classList.add("collapsed")
+      }
+      else {
+        childrenElement.classList.remove("collapsed")
+        el.classList.remove("collapsed")
+      }
+    },
+    get children() { return children },
+    add(childHtml: string, index = -1) {
+      const childElement = compile(childHtml)
+      if (index > -1 && index < childrenElement.childElementCount) {
+        childrenElement.children[index].before(childElement)
+      }
+      else {
+        childrenElement.append(childElement)
+      }
+      setChildren()
+      addListeners(this)
+    },
+    remove(index: number) {
+      if (index < 0 || index >= childrenElement.childElementCount) return
+      else {
+        const child = childrenElement.children[index]
+        childrenElement.removeChild(child)
+        setChildren()
+        addListeners(this)
+      }
+    },
+    _el: el
+  }
+}
+const addListeners = (root: TreeNode) => {
+  const children = root.children
+  const bullets = [...root._el.children].slice(0,2)
+  for (const bullet of bullets) {
+    if (children.length > 0) {
+      (bullet as HTMLSpanElement).onclick = () => { root.collapsed = !root.collapsed }
+    }
+    else {
+      (bullet as HTMLSpanElement).onclick = null
+    }
+  }
+  for (const child of children) addListeners(child)
+  return root
+}
+
+export const toTreeData = (node: TreeNode): TreeData =>  {
+  const { content, children } = node
+  if (children.length > 0) return [content, children.map(toTreeData)]
+  else return content
+}
+export const treeNodeHtml = (params: Partial<TreeParams>) => (content: string, ...children: string[]) => {
+  const parameters = Object.assign({}, defaults, params)
+  return `
+    <div class="tree_node">
+      <span class="collapsed bullet">${parameters.collapsedBullet}</span>
+      <span class="expanded bullet">${parameters.expandedBullet}</span>
+      <span class="content">${content}</span>
+      <div class="children">
+          ${children.join("")}
+      </div>
+    </div>
+  `.replace(/\n\s*/g,"")
+}
+export const tree = (html: string, params: Partial<TreeParams> = {}): Tree => {
+  const parameters = Object.assign({}, defaults, params)
+  const { indent, id } = parameters
+  const style = `
+    <style>
+      .tree_node {
+        margin-left: ${indent};
+        padding-right: ${indent};
+        padding-top: 0.2em;
+        padding-bottom: 0.2em;
+        font-family: ${parameters.fontFamily};
+        font-size: ${parameters.fontSize};
+        font-weight: ${parameters.fontWeight};
+      }
+      .collapsed.bullet {
+        visibility: hidden;
+        font-size: 0;
+        width: 0;
+      }
+      .expanded.bullet {
+        visibility: visible;
+        width: initial;
+        font-size: inherit;
+      }
+      .tree_node.collapsed > .collapsed.bullet {
+        visibility: inherit;
+        width: initial;
+        font-size: inherit;
+      }
+      .tree_node.collapsed > .expanded.bullet {
+        visibility: hidden;
+        font-size: 0;
+        width: 0;
+      }
+      .children.collapsed { visibility: hidden; height: 0; }
+      .children.collapsed * { visibility: hidden; }
+    </style>
+  `
+  const openingTag = `<div class="tree"${id ? `id="${id}"` : ""}>`
+  const el = compile(`${openingTag}${style}${html}</div>`)
+  const root = treeNode(el.querySelector(".tree_node") as HTMLDivElement)
+
+  if (parameters.baseFontSize) root._el.style.fontSize = parameters.baseFontSize
+
+  addListeners(root)
+
+  return {
+    mount(parent: Element) {
+      parent.append(el)
+    },
+    unmount() {
+      el.remove()
+    },
+    root
+  }
+}
