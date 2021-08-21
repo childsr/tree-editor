aee40dc3e26f4d8ca04e6579e69b5c025f2d4c4c
Ryan Childs <rchilds24@gmail.com>
2021-08-21 13:04:44 -0600

    update Tree and TreeNode interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 export interface Tree {
+  readonly root: TreeNode
+  setRoot(newRoot: TreeNode): void
   mount(parent: Element): void
   unmount(): void
-  root: TreeNode
+  readonly _el: HTMLDivElement
 }
 export interface TreeNode {
   content: string
   collapsed: boolean
   readonly children: TreeNode[]
-  add(childHtml: string, index?: number): void
+  add(childData: TreeData, index?: number): void
   remove(index: number): void
-  _el: HTMLDivElement
+  getTreeData(): TreeData
+  readonly _el: HTMLDivElement
 }
-export type TreeParams = {
+export type TreeData = string | [content: string, children: TreeData[]]
+
+export interface TreeParams {
   indent: string
   fontFamily: string
   fontWeight: string
@@ -21,8 +26,7 @@ export type TreeParams = {
   collapsedBullet: string
   expandedBullet: string
 }
-export type TreeData = string | [content: string, children: TreeData[]]
- 
+
 const defaults: Partial<TreeParams> = {
   indent: "10px",
   fontFamily: "monospace",
@@ -37,12 +41,12 @@ const compile = (html: string): HTMLElement => {
   div.innerHTML = html
   return div.children[0] as HTMLElement
 }
-const treeNode = (el: HTMLDivElement): TreeNode => {
+const treeNode = (params: Partial<TreeParams>) => (el: HTMLDivElement): TreeNode => {
   const childrenElement = el.querySelector(".children") as HTMLDivElement
   const contentEl = el.querySelector(".content") as HTMLSpanElement
   let content = contentEl.innerText
   let children: TreeNode[]
-  const setChildren = () => { children = [...childrenElement.children].map(treeNode as any) as TreeNode[] }
+  const setChildren = () => { children = [...childrenElement.children].map(treeNode (params) as any) as TreeNode[] }
   setChildren()
   return {
     get content() { return content },
@@ -62,8 +66,8 @@ const treeNode = (el: HTMLDivElement): TreeNode => {
       }
     },
     get children() { return children },
-    add(childHtml: string, index = -1) {
-      const childElement = compile(childHtml)
+    add(childData: TreeData, index = -1) {
+      const childElement = compile(treeDataToHtml (params) (childData))
       if (index > -1 && index < childrenElement.childElementCount) {
         childrenElement.children[index].before(childElement)
       }
@@ -83,6 +87,7 @@ const treeNode = (el: HTMLDivElement): TreeNode => {
         addListeners(this)
       }
     },
+    getTreeData() { return treeNodeToTreeData(this) },
     _el: el
   }
 }
@@ -113,16 +118,12 @@ const removeListeners = (root: TreeNode) => {
 }
 
 
-const createTreeNodeHtml = (params: Partial<TreeParams>) => (data: TreeData): string => {
-  if (typeof data === "string") {
-    return treeNodeHtml(params)(data)
-  }
-  else {
-    const [content, children] = data
-    return treeNodeHtml(params)(content, ...children.map(createTreeNodeHtml(params)))
-  }
+const treeNodeToTreeData = (node: TreeNode): TreeData => {
+  const { content, children } = node
+  if (children.length > 0) return [content, children.map(treeNodeToTreeData)]
+  else return content
 }
-const treeNodeHtml = (params: Partial<TreeParams>) => (content: string, ...children: string[]) => {
+const genTreeNodeHtml = (params: Partial<TreeParams>) => (content: string, ...children: string[]) => {
   const parameters = Object.assign({}, defaults, params)
   return `
     <div class="tree_node">
@@ -135,7 +136,41 @@ const treeNodeHtml = (params: Partial<TreeParams>) => (content: string, ...child
     </div>
   `.replace(/\n\s*/g,"")
 }
-const tree = (html: string, params: Partial<TreeParams> = {}): Tree => {
+const treeDataToHtml = (params: Partial<TreeParams>) => (data: TreeData): string => {
+  if (typeof data === "string") {
+    return genTreeNodeHtml(params)(data)
+  }
+  else {
+    const [content, children] = data
+    return genTreeNodeHtml(params)(content, ...children.map(treeDataToHtml(params)))
+  }
+}
+// const genTreeNodeElement = (params: Partial<TreeParams>) => (data: TreeData) => compile(treeDataToHtml (params) (data)) as HTMLDivElement
+// const treeNodeToHtml = (params: Partial<TreeParams>) => (node: TreeNode): string => (
+//   treeDataToHtml (params) (treeNodeToTreeData (node))
+// )
+// const treeDataToTreeNode = (params: Partial<TreeParams>) => (data: TreeData) => {
+//   return treeNode (params) (genTreeNodeElement (params) (data))
+// }
+/** Ensure all child html elements have been appended to their parent elements. */
+const appendAll = (node: TreeNode) => {
+  const { children, _el } = node
+  if (children.length === 0) return
+  const parent = _el.querySelector(".children") as HTMLDivElement
+  if (parent.childElementCount < children.length) {
+    for (const childElement of parent.children) {
+      childElement.remove()
+    }
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i]
+      parent.append(child._el)
+    }
+  }
+  for (const child of children) {
+    appendAll (child)
+  }
+}
+const tree = (html: string, params: Partial<TreeParams>): Tree => {
   const parameters = Object.assign({}, defaults, params)
   const { indent, id } = parameters
   const style = `
@@ -174,34 +209,47 @@ const tree = (html: string, params: Partial<TreeParams> = {}): Tree => {
     </style>
   `
   const openingTag = `<div class="tree"${id ? `id="${id}"` : ""}>`
-  const el = compile(`${openingTag}${style}${html}</div>`)
-  const root = treeNode(el.querySelector(".tree_node") as HTMLDivElement)
+  const outerHtml = (inner: string) => `${openingTag}${style}${inner}</div>`
+  const el = compile(outerHtml(html)) as HTMLDivElement
+  let root = treeNode (params) (el.querySelector(".tree_node") as HTMLDivElement)
 
   if (parameters.baseFontSize) root._el.style.fontSize = parameters.baseFontSize
 
-  addListeners(root)
-
+  let mounted = false
+  
   return {
     mount(parent: Element) {
-      parent.append(el)
+      if (!mounted) {
+        mounted = true
+        parent.append(el)
+        addListeners(root)
+      }
     },
     unmount() {
-      el.remove()
+      if (mounted) {
+        mounted = false
+        el.remove()
+        removeListeners(root)
+      }
     },
-    root
+    setRoot(newRoot: TreeNode) {
+      removeListeners(root)
+      root._el.remove()
+      root = newRoot
+      el.append(newRoot._el)
+      appendAll(newRoot)
+      addListeners(newRoot)
+    },
+    get root() { return root },
+    _el: el
   }
 }
-const nodeToTreeData = (node: TreeNode): TreeData =>  {
-  const { content, children } = node
-  if (children.length > 0) return [content, children.map(nodeToTreeData)]
-  else return content
-}
 
-export const toTreeData = (tree: Tree): TreeData => nodeToTreeData(tree.root)
-export const createTree = (data: TreeData, params: Partial<TreeParams>): Tree => {
-  return tree(createTreeNodeHtml (params) (data), params)
+export const createTree = (data: TreeData, params: Partial<TreeParams> = {}): Tree => {
+  return tree(treeDataToHtml (params) (data), params)
 }
-export const treeData = (content: string, ...children: TreeData[]): TreeData => {
+/** Helper function to make writing out `TreeData` a bit easier.  */
+export const t = (content: string, ...children: TreeData[]): TreeData => {
   if (children.length === 0) return content
   else return [content, children]
 }
